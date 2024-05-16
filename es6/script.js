5d9f17534eae8a795955b444996106151187b434
muirurimukundi <themasalacollective@gmail.com>
2024-05-16 11:56:35 -0400

    let vs var, let creates local scope

--- es6/script.js
+++ es6/script.js
@@ -18,4 +18,52 @@ window.onload = function(){
     calculateArea(5);
     console.log("Global pi is", pi); // uses the global pi thus its 3.142
 
+
+    /*----------------------------------------------------------------*/
+    // Let has local scope / lexical scope - 
+
+
+    var x = 10; 
+
+    if (x > 5){
+    var x = 5;
+    }
+
+    console.log("Value of x is ", x) 
+
+    var y = 20;
+
+    if (y > 5){
+        let y = 10;
+        console.log("Inside scope created by {}, the value of y is", y)
+    }
+
+    console.log("Inside outer scope the value of y is ", y)
+    console.log("Notice we created the variable with var but changed it locally with let.")
+
+
+    var items = document.getElementsByTagName("li");
+    console.log(items)
+
+    for(var b=0; b < items.length; b++){
+
+        items[b].onclick=function(){
+            console.log(b);
+        }
+    }
+
+    // it will log out 4 because var is created in the global scope , creating a global variable 
+    // remember you have to click to notice the console logs]
+    
+    var newitems = document.getElementsByTagName('li');
+    console.log('Newitems: ', newitems);
+
+    for(let y=0; y<newitems.length; y++){
+        
+        newitems[y].onclick=function(){
+            console.log(y);
+        }
+    }
+
+
 }
