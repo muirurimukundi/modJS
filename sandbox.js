fef255445fd6c5abf319599174f1c9b7d5d36f04
muirurimukundi <themasalacollective@gmail.com>
2024-05-16 06:51:35 -0400

    variables, let and const

--- sandbox.js
+++ sandbox.js
@@ -3,3 +3,17 @@ console.log("today is Thursday");
 console.log(1); // outputting something to the console
 
 // Variables 
+
+let age = 25;
+let year = 2024; 
+
+console.log(age, year);
+
+age = 30;
+
+console.log("The age is ", age);
+
+const points = 100;  // constant's values don't change
+
+console.log("You scored ", points, " points");
+points = 50; // wono't allow you to change or overwrite the value
