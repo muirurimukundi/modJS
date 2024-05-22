021390582e845c9d6bcda3b1d4c5052ca231c9c6
muirurimukundi <themasalacollective@gmail.com>
2024-05-22 18:06:59 -0400

    color flipper code using random array selection

--- projects/background-switch/app.js
+++ projects/background-switch/app.js
@@ -3,9 +3,29 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById("flipper-btn");
-const colorLabel = document.querySelector("span #color");
+const colorLabel = document.querySelector("#color");
 
+console.log(colorLabel);
 btn.addEventListener('click',function(){
-    const randomNumber = 2;
-    document.body.style.background = colors[randomNumber];
-})
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber];
+    colorLabel.textContent = colors[randomNumber];
+});
+
+
+// function to generate a random number
+
+function getRandomNumber(){
+    // Math.random returns value between 0 and 1 
+    // random multiplied by 4 gives results btwn 0 and 3.sth but not upto 4
+    // Math.floor rounds down to nearest integer 
+    return Math.floor(Math.random() * colors.length);
+}
+
+/*
+How to get a random number in jS
+Attaching an event using an event listener and anonymous function 
+
+
+*/
